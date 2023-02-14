f43c3e8fe566f969a0ff7b973ee18c5a1fb10c3a
jinwoo <hongdongdong3012@gmail.com>
2023-02-14 15:46:57 +0900

    퍼블

---
+++ component/LoginModal.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styles from '/component/LoginModal.module.css'
+
+
+const LoginModal = () => (
+	<div className={styles.LoginModal_wrap}>
+		<div className={styles.h2_wrap}>
+			<h2>asd</h2>
+			<p>asdasd</p>
+		</div>
+	</div>
+);
+
+export default LoginModal;
